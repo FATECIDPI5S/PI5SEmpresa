bf5ef8239959dd5ad28f47872534721a901a6c52
Jean Souza <jean.carlos.nmk@gmail.com>
2018-11-05 04:12:08 -0200

    Adicionando telas de login e cadastro sem funcionalidades implementadas.

---
+++ app/src/main/java/br/com/icomidaempresa/LoginActivity.java
@@ -0,0 +1,24 @@
+package br.com.icomidaempresa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void abrirHome(View v){
+        finish();
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void abrirSignUp(View v){
+        startActivity(new Intent(this, SignUpActivity.class));
+    }
+}
