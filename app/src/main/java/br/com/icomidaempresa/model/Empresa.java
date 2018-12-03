13714f9892e68249fac4238f5051cf44760136ae
Jean <jean.carlos.nmk@gmail.com>
2018-12-03 01:31:57 -0200

    Activity Empresa e Fragment Empresa atualizados com novo layout.

---
+++ app/src/main/java/br/com/icomidaempresa/model/Empresa.java
@@ -0,0 +1,67 @@
+package br.com.icomidaempresa.model;
+
+public class Empresa {
+    private String RazaoSocial;
+    private String NomeFantasia;
+    private String CNPJ;
+    private String IE;
+    private String Endereco;
+    private String Telefone;
+    private String Celular;
+
+    public String getRazaoSocial() {
+        return RazaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        RazaoSocial = razaoSocial;
+    }
+
+    public String getNomeFantasia() {
+        return NomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        NomeFantasia = nomeFantasia;
+    }
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    public String getIE() {
+        return IE;
+    }
+
+    public void setIE(String IE) {
+        this.IE = IE;
+    }
+
+    public String getEndereco() {
+        return Endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        Endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
+
+    public String getCelular() {
+        return Celular;
+    }
+
+    public void setCelular(String celular) {
+        Celular = celular;
+    }
+}
--- app/src/main/java/br/com/icomidaempresa/view/fragment/EmpresaFragment.java
+++ app/src/main/java/br/com/icomidaempresa/view/fragment/EmpresaFragment.java
@@ -8,11 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import br.com.icomidaempresa.R;
+import br.com.icomidaempresa.model.Empresa;
 import br.com.icomidaempresa.view.activity.EmpresaActivity;
 
 
@@ -20,54 +23,86 @@ import br.com.icomidaempresa.view.activity.EmpresaActivity;
  * A simple {@link Fragment} subclass.
  */
 public class EmpresaFragment extends Fragment {
-
     GridLayout glEmpresaDescricao;
-    ImageButton imgbMostrarDescricao;
+    FloatingActionButton fabEmpresa;
+    ImageButton imgbMostrarEmpresa;
+    ImageButton imgbAlterarEmpresa;
+    TextView tvCVEmpresaTitulo;
+    TextView tvCVEmpresaSubtitulo;
+    TextView tvRazaoSocial;
+    TextView tvNomeFantasia;
+    TextView tvCNPJ;
+    TextView tvIE;
+    TextView tvEndereco;
+    TextView tvTelefone;
+    TextView tvCelular;
 
     public EmpresaFragment() {
-        // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_empresa, container, false);
 
-        FloatingActionButton fabEmpresa =  view.findViewById(R.id.fabEmpresa);
-        glEmpresaDescricao =  view.findViewById(R.id.glEmpresaDescricao);
-        imgbMostrarDescricao =  view.findViewById(R.id.imgbMostrarDescricao);
+        glEmpresaDescricao = view.findViewById(R.id.glEmpresaDescricao);
+        fabEmpresa = view.findViewById(R.id.fabEmpresa);
+        imgbMostrarEmpresa = view.findViewById(R.id.imgbMostrarEmpresa);
+        imgbAlterarEmpresa = view.findViewById(R.id.imgbAlterarEmpresa);
+        tvCVEmpresaTitulo = view.findViewById(R.id.tvCVEmpresaTitulo);
+        tvCVEmpresaSubtitulo = view.findViewById(R.id.tvCVEmpresaSubtitulo);
+        tvRazaoSocial = view.findViewById(R.id.tvRazaoSocial);
+        tvNomeFantasia = view.findViewById(R.id.tvNomeFantasia);
+        tvCNPJ = view.findViewById(R.id.tvCNPJ);
+        tvIE = view.findViewById(R.id.tvIE);
+        tvEndereco = view.findViewById(R.id.tvEndereco);
+        tvTelefone = view.findViewById(R.id.tvTelefone);
+        tvCelular = view.findViewById(R.id.tvCelular);
 
-        glEmpresaDescricao.setVisibility(View.GONE);
+        fabEmpresa.setOnClickListener(this::abrirEmpresaActivity);
+        imgbMostrarEmpresa.setOnClickListener(this::verificarDescricao);
+        imgbAlterarEmpresa.setOnClickListener(this::alterarEmpresa);
 
-        imgbMostrarDescricao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(glEmpresaDescricao.getVisibility() == View.VISIBLE){
-                    esconderDescricao();
-                }else if(glEmpresaDescricao.getVisibility() == View.GONE){
-                    mostrarDescricao();
-                }
-            }
-        });
-
-        fabEmpresa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getContext(), EmpresaActivity.class));
-            }
-        });
+        popularEmpresa();
 
         return view;
     }
 
-    public void esconderDescricao(){
-        imgbMostrarDescricao.setImageResource(R.drawable.ic_keyboard_arrow_down_black_50dp);
-        glEmpresaDescricao.setVisibility(View.GONE);
+    private void alterarEmpresa(View view) {
+        startActivity(new Intent(getContext(), EmpresaActivity.class));
     }
 
-    public void mostrarDescricao(){
-        imgbMostrarDescricao.setImageResource(R.drawable.ic_keyboard_arrow_up_black_50dp);
-        glEmpresaDescricao.setVisibility(View.VISIBLE);
+    private void verificarDescricao(View view) {
+        if (glEmpresaDescricao.getVisibility() == View.VISIBLE) {
+            imgbMostrarEmpresa.setImageResource(R.drawable.ic_keyboard_arrow_down_black_50dp);
+            glEmpresaDescricao.setVisibility(View.GONE);
+        } else if (glEmpresaDescricao.getVisibility() == View.GONE) {
+            imgbMostrarEmpresa.setImageResource(R.drawable.ic_keyboard_arrow_up_black_50dp);
+            glEmpresaDescricao.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void abrirEmpresaActivity(View view) {
+        startActivity(new Intent(getContext(), EmpresaActivity.class));
+    }
+
+    private void popularEmpresa() {
+        Empresa empresa = new Empresa();
+        empresa.setRazaoSocial("FATEC RESTAURANT LTDA");
+        empresa.setNomeFantasia("Restaurante FATEC");
+        empresa.setCNPJ("00.755.140/0001-70");
+        empresa.setIE("061.811.922.200");
+        empresa.setEndereco("R. Dom Pedro I, 65 - Cidade Nova I, Indaiatuba - SP, 13334-100");
+        empresa.setTelefone("(19) 3885-1923");
+        empresa.setCelular("(19) 99170-7070");
+
+        tvCVEmpresaTitulo.setText(empresa.getNomeFantasia());
+        tvCVEmpresaSubtitulo.setText(empresa.getCNPJ());
+        tvRazaoSocial.setText(empresa.getRazaoSocial());
+        tvNomeFantasia.setText(empresa.getNomeFantasia());
+        tvCNPJ.setText(empresa.getCNPJ());
+        tvIE.setText(empresa.getIE());
+        tvEndereco.setText(empresa.getEndereco());
+        tvTelefone.setText(empresa.getTelefone());
+        tvCelular.setText(empresa.getCelular());
     }
 }
